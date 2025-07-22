7249c80220e3fcf246c7c123b3e4489cb179983a
ANDREW NOTERMANS <98767705+andrewnotermans@users.noreply.github.com>
2025-07-22 20:52:16 +0200

    add navigation menu and scrollbar styles typographix project

--- typographix/script.js
+++ typographix/script.js
@@ -37,3 +37,18 @@ if (currentThemeFromLocalStorage) {
         toggleLightMode();
     }
 }
+
+// Navigation
+
+const nav = document.getElementById('nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenu() {
+  nav.classList.toggle("active");
+  menuIcon.classList.toggle("active");
+}
+
+function hideMenu() {
+    nav.classList.remove('active');
+    menuIcon.classList.remove('active');
+}
